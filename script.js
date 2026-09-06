0904c6dcfe11a779eb2620f55bf20a253d4ae066
slime786 <moheenmahmood@hotmail.co.uk>
2026-09-06 13:56:49 +0100

    Delete script.js

--- script.js
+++
@@ -1,1 +0,0 @@
-document.querySelectorAll('a[href^="#"]').forEach(link=>{link.addEventListener('click',e=>{const el=document.querySelector(link.getAttribute('href'));if(el){e.preventDefault();el.scrollIntoView({behavior:'smooth'})}})});
